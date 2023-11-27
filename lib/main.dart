2f585166c878820abf2fe3667b65855132108e7b
Christian Afreh <christian.afreh@primustechgh.net>
2023-11-27 19:50:27 +0000

    added initial project files

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Center(
+            child: Text('Dicee'),
+          ),
+        ),
+        body: DiceePage(),
+      ),
+    );
+  }
+}
+
+class DiceePage extends StatelessWidget {
+  DiceePage({super.key});
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 3;
+
+  void throwDice() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+    print(leftDiceNumber);
+    print(rightDiceNumber);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              child: Image.asset('assets/images/dice$leftDiceNumber.png'),
+              onPressed: () => throwDice(),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              child: Image.asset('assets/images/dice$rightDiceNumber.png'),
+              onPressed: () => throwDice(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
